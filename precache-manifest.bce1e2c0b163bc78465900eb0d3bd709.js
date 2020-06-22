db603ad563032b104ad43a93721aeda11fe48e21
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-06-23 00:25:07 +0100

    Updates

---
+++ precache-manifest.bce1e2c0b163bc78465900eb0d3bd709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de58a86f312b97d63b8ef5ada3eee3a0",
+    "url": "/polyglot/index.html"
+  },
+  {
+    "revision": "37d312484947a9446eea",
+    "url": "/polyglot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/polyglot/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/polyglot/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d312484947a9446eea",
+    "url": "/polyglot/static/js/main.ed360ce8.chunk.js"
+  },
+  {
+    "revision": "8bfde57a94458bc4abf7",
+    "url": "/polyglot/static/js/runtime-main.3cc1ff58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/polyglot/static/media/logo.5d5d9eef.svg"
+  }
+]);
